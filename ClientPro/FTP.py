b964a18e643ca2f3072b5a274503d6738cb91a8b
zhanggao <zgzg881215@126.com>
2018-01-31 23:28:39 +0800

    python ftplib中FTP的学习和使用

---
+++ ClientPro/FTP.py
@@ -0,0 +1,43 @@
+import ftplib
+import os
+import socket
+
+HOST = '127.0.0.1'
+DIRN = 'file'
+FILE = 'ftp.txt'
+
+
+def ftp():
+    f = ftplib.FTP(HOST)
+    print('Connected to HOST : "%s"' % HOST)
+    try:
+        f.login()
+    except ftplib.error_perm:
+        print('Cannot login "%s"' % HOST)
+    try:
+        f.cwd(DIRN)
+    except ftplib.error_perm:
+        print('Cannot CD to "%s"' % DIRN)
+        f.quit()
+        return
+    print('change to "%s"' % DIRN)
+    f.pwd()
+    try:
+        loc = open(FILE, 'wb').write
+        f.retrbinary('RETE %s' % FILE, loc)   # 报错，不能读取文件
+    except ftplib.error_perm:
+        print('error is : %s ' % ftplib.error_perm)
+        print('cannot read file "%s"' % FILE)
+        # os.unlink(FILE)
+    else:
+        print('downloaded "%s" to CWD' % FILE)
+    f.quit()
+
+
+def wirteFile(data):
+    with open(FILE, 'wb') as fobj:
+        fobj.write(data)
+
+
+if __name__ == '__main__':
+    ftp()
